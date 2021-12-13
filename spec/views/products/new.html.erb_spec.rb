3ff954d3c5bbe7a67a4e570e69c25a7cece47c1d
Nicholas Kirby <nkirby@nxtsoft.com>
2021-12-13 14:10:55 -0500

    add products; and railsroutes

---
+++ spec/views/products/new.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "products/new", type: :view do
+  before(:each) do
+    assign(:product, Product.new(
+      name: "MyString",
+      description: "MyText",
+      price: "9.99",
+      image_url: "MyString"
+    ))
+  end
+
+  it "renders new product form" do
+    render
+
+    assert_select "form[action=?][method=?]", products_path, "post" do
+
+      assert_select "input[name=?]", "product[name]"
+
+      assert_select "textarea[name=?]", "product[description]"
+
+      assert_select "input[name=?]", "product[price]"
+
+      assert_select "input[name=?]", "product[image_url]"
+    end
+  end
+end
