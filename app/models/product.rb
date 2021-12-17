c359aa4bf716dd100636fdb3ed58dcd676c1fadf
Nicholas Kirby <nkirby@nxtsoft.com>
2021-12-17 12:22:13 -0500

    add actiontext; foundation for product turbo frames

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
-end
+  has_rich_text :description
+
+  after_create_commit { broadcast_prepend_to :products }
+  after_update_commit { broadcast_replace_to :products }
+  after_destroy_commit { broadcast_remove_to :products }
+ end
