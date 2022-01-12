55147ae0de9ba46318ab8fcc9c8101845baae446
Nicholas Kirby <nkirby@nxtsoft.com>
2022-01-12 11:49:54 -0500

    add role to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_183337) do
+ActiveRecord::Schema.define(version: 2022_01_12_144134) do
 
   create_table "action_mailbox_inbound_emails", force: :cascade do |t|
     t.integer "status", default: 0, null: false
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2021_12_29_183337) do
     t.datetime "remember_created_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
