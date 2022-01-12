0c94217e7257f0878091c25bef9f28a090dd6d6e
Nicholas Kirby <nkirby@nxtsoft.com>
2022-01-12 13:41:45 -0500

    add customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+  has_many :orders
+
+  after_create_commit { broadcast_prepend_to :customers }
+  after_update_commit { broadcast_replace_to :customers }
+  after_destroy_commit { broadcast_remove_to :customers }
 end
